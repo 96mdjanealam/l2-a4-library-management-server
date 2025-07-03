7e6039dc7eaa047d1b152a8bc87954f946bf0d00
96mdjanealam <96mdjanealam@gmail.com>
2025-07-03 13:49:17 +0600

    cmt 2

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express, { Application, Request, Response } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
-
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(cors({origin: "http://localhost:5173"}));
 
-
-app.use("/books", bookRoutes)
-app.use("/borrow", borrowRoutes)
-
+app.use("/books", bookRoutes);
+app.use("/borrow", borrowRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library Management Server");
